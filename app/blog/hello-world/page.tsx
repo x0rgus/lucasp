c6ac1b72210e1a2ecce798042ed64c25e8fab16f
x0rgus <lucas.pr1@hotmail.com>
2026-03-15 18:50:01 -0300

    add: some pages

---
+++ app/blog/hello-world/page.tsx
@@ -0,0 +1,8 @@
+export default function Post() {
+  return (
+    <main style={{ padding: "40px" }}>
+      <h1>hello world</h1>
+      <p> Testing my blog :)</p>
+    </main>
+  )
+}
---
+++ app/blog/page.tsx
@@ -0,0 +1,11 @@
+export default function Blog() {
+  return (
+    <main style={{ padding: "40px" }}>
+      <h1>blog</h1>
+
+      <p>
+        <a href="/blog/hello-world">hello world</a>
+      </p>
+    </main>
+  )
+}
